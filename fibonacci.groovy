4ee42674684377d67e742a0775396dc037f9df32
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-03-30 17:20:09 -0600

    adding class fibonacci groovy

---
+++ fibonacci.groovy
@@ -0,0 +1,12 @@
+public class fibonacci {
+	public static void main(String[] args) {
+		int valor1=0,valor2=1,resultado,iteracion=10;
+
+		for (int inicio=0; inicio < iteracion; inicio++) {
+			resultado=valor1+valor2;
+			valor1=valor2;
+			valor2=resultado;
+			System.out.println(" "+resultado);
+		}
+	}
+}
