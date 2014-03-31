31c1526dd1ce5bd0a8b1aa8a1d63348f18e7dfd4
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-03-30 18:43:59 -0600

    remove type int by dynamyc typing

--- fibonacci.groovy
+++ fibonacci.groovy
@@ -1,7 +1,7 @@
 class fibonacci {
 
 	public static void main(String[] args) {
-		int valor1=0,valor2=1,resultado,iteracion=10
+		def valor1=0,valor2=1,resultado,iteracion=10
 
 		for (inicio in 1 .. iteracion) {
 			resultado=valor1+valor2
