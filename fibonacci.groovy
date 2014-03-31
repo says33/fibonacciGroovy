ed05669b4b6f43c747b83862b9b5193554d07bf2
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-03-30 18:17:09 -0600

    remove ; element

--- fibonacci.groovy
+++ fibonacci.groovy
@@ -1,13 +1,13 @@
 class fibonacci {
 
 	public static void main(String[] args) {
-		int valor1=0,valor2=1,resultado,iteracion=10;
+		int valor1=0,valor2=1,resultado,iteracion=10
 
 		for (int inicio=0; inicio < iteracion; inicio++) {
-			resultado=valor1+valor2;
-			valor1=valor2;
-			valor2=resultado;
-			println(" "+resultado);
+			resultado=valor1+valor2
+			valor1=valor2
+			valor2=resultado
+			println(" "+resultado)
 		}
 	}
 }
