cbf069073ba9ce088610ed0e25ee45420de4331c
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-03-30 18:45:53 -0600

    remove sentence for by range and each

--- fibonacci.groovy
+++ fibonacci.groovy
@@ -3,7 +3,7 @@ class fibonacci {
 	public static void main(String[] args) {
 		def valor1=0,valor2=1,resultado,iteracion=10
 
-		for (inicio in 1 .. iteracion) {
+		(1..iteracion).each {
 			resultado=valor1+valor2
 			valor1=valor2
 			valor2=resultado
