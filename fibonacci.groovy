48f14596181a6d091ec6b02e254657bf3ccff2f8
unknown <sergio.rodriguez@NEZA7100R9D956L.ac.sep.gob.mx>
2014-04-02 18:47:38 -0600

    delete main method and class declaration and modification to script groovy

--- fibonacci.groovy
+++ fibonacci.groovy
@@ -1,13 +1,11 @@
-class fibonacci {
-
-	public static void main(String[] args) {
-		def valor1=0,valor2=1,resultado,iteracion=10
-
-		(1..iteracion).each {
-			resultado=valor1+valor2
-			valor1=valor2
-			valor2=resultado
-			println(" "+resultado)
-		}
+def fibonacci() {
+	def valor1=0,valor2=1,resultado,iteracion=10
+	(1..iteracion).each {
+		resultado=valor1+valor2
+		valor1=valor2
+		valor2=resultado
+		println(" "+resultado)
 	}
 }
+
+fibonacci()
